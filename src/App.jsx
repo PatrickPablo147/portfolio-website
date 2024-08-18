3ba6c8f9e6a6577c8647e2085bb0e8f9a826c9b5
Patrick Pablo <114048939+PatrickPablo147@users.noreply.github.com>
2024-08-18 19:45:40 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './component/Hero'
+import Navbar from './component/Navbar'
+import Skills from './component/skills'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Skills/>
+    </>
+  )
+}
+
+export default App
